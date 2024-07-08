c017099fec38e6e3ed4f02dc6a8d640b7d72696e
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2024-07-08 23:36:35 +0530

    Add files via upload

---
+++ .devcontainer/01_basics/functions.js
@@ -0,0 +1,36 @@
+// HITESH CHOUDHARY
+
+function kitkat () {
+    console.log("Happy happy happy");
+}
+
+kitkat ()
+
+
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("s");
+    console.log("h");
+}
+
+// sayMyName()
+
+//function addTwoNumbers(number1 , number2) {     //parameters
+//    console.log(number1 + number2);
+// }
+
+
+function addTwoNumbers(number1 , number2) {     //parameters
+
+    // let result = number1 +  number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)    //arguments
+
+ console.log("Result:", result);
