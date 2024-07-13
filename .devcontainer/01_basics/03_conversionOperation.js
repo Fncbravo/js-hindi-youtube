f85ad3b06ab42c922569395cb2b7a5996e0c250f
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2024-07-13 09:44:30 +0530

    Update 03_conversionOperation.js

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -29,3 +29,40 @@ console.log(typeof stringNumber);
 
 // Conversion is really easy you'all
 // Just assign a variable then assign another variable & = what you want to convert it into (1st variable)
+
+
+// ********************Operations********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3); used mostly
+
+let str1 = "Hello"
+let str2 = " sujeet"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log( 1 + "2");  
+// console.log( "1" + 2 + 2);          // if string is first then all will be treated as string
+// console.log( 1 + 2 + "2");          // if string is last then first operation will be done then it'll come to string
+// console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100 
+++gameCounter;                   // prefix & postfix
+console.log(gameCounter);
