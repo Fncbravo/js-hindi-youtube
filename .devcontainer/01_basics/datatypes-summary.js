3590d1179fd53d02ecb2f2b2ad7debd0401b1dfc
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2024-07-13 12:11:32 +0530

    Update datatypes-summary.js

--- .devcontainer/01_basics/datatypes-summary.js
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -35,3 +35,29 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive), Heap(Non Primitive)
+
+let myYoutubename = "Serrioton"
+
+let anothername = myYoutubename
+anothername = "FNCBravo"
+
+console.log(anothername);
+console.log(myYoutubename);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "sujeet@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
