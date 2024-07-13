5a4c3fcdaaf7680f17d070a35b134c52ee3fab12
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2024-07-13 08:56:43 +0530

    Update 03_conversionOperation.js

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -26,3 +26,6 @@ let someNumber = 33
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// Conversion is really easy you'all
+// Just assign a variable then assign another variable & = what you want to convert it into (1st variable)
