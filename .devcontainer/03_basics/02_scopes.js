af2ef7a65e88c715ede3492052b319dfb9958d3e
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2024-08-03 23:58:25 +0530

    Update 02_scopes.js

--- .devcontainer/03_basics/02_scopes.js
+++ .devcontainer/03_basics/02_scopes.js
@@ -15,3 +15,39 @@ console.log(a);
 // console.log(c);
 
 // Node global scope & global scope inn console in browser are both different
+
+// NESTED SCOPES
+
+function one() {
+    const username = "sujeet"
+
+    function two() {
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+    // two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "sujeet"
+    if (username === "sujeet") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// +++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++
+
+function addone (num) {
+    return num + 1
+}
+
+addone(5)
