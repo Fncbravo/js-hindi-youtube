bd38b726ee5214d1b20e436d3e0e2b013b0d0d41
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2024-08-04 01:04:18 +0530

    Update 04_iife.js

--- .devcontainer/03_basics/04_iife.js
+++ .devcontainer/03_basics/04_iife.js
@@ -1,11 +1,27 @@
-// Immediately Invoked Function Expressions (IIFE)
+// Immediately Invoked Function Expression (IIFE)
 
 
-(function chai(){
+(function chai () {
     // named IIFE
     console.log(`DB CONNECTED`);
 })();
 
+// ()()      // Just wrap the function in paranthesis & we were using paranthesis before to call the funciton so just put that there empty
+           // first one is func definition and second one is execution call 
+
+
 ( (name) => {
-    console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+console.log(`DB CONNECTED TWO ${name}`);
+} )('sujeet')           
+
+// named iife - normal funtiino declaration
+// unnamed iife without name arrow fucntion
+// parameters passing - on line 13 parameters are passed
+// Two iifes together -  ; semicolon expected
+
+// iffe -- immediately executution
+// Interview -- Global scope pollution causes problems so to remove that we use iife
+
+
+
+
