8ea82e45de5ad877145e56e7ee181b62ed3c8249
Sujeet Baisane <118635012+Fncbravo@users.noreply.github.com>
2024-08-09 16:14:20 +0530

    Update nine.js

--- .devcontainer/05_iterations/nine.js
+++ .devcontainer/05_iterations/nine.js
@@ -1,33 +1,54 @@
+// Javascript reduce method MDN
+
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialvalue = 0;
+const sumWithInitial = array1.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialvalue
+);
+
+console.log(sumWithInitial);
+// Expected output: 10
+
+// ----------------------------------------------------------------------------------------------
+
 const myNums = [1, 2, 3]
 
 // const myTotal = myNums.reduce(function (acc, currval) {
-//   console.log(`acc: ${acc} and currva: ${currval}`);
-//   return acc + currval
+//     console.log(`acc: ${acc} and currval: ${currval}` );
+    
+//     return acc + currval
 // }, 0)
 
-const myTotal = myNums.reduce( (acc, curr) => (acc+curr), 0)
+// console.log(myTotal);
+
+//  Now same with arrow function
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
 
 console.log(myTotal);
 
-const shoppingCart = {
-  { 
-      itemName: "js course"
-      price: 2999
-  },
-  { 
-      itemName: "py course"
-      price: 999
-  },
-  { 
-      itemName: "mobile dev course"
-      price: 5999
-  },
-  { 
-      itemName: "data science course"
-      price: 12999
-  },
-}
+const shoppinCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
 
-const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
+const priceToPay = shoppinCart.reduce( (acc, item) => acc + item.price, 0)
 
 console.log(priceToPay);
